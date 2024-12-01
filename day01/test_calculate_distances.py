67a0aab214f5b1e2e4e27015046d6ea49118ad42
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-01 10:24:51 +0100

    Named back, as part 2 uses same data. duh

--- day01/test_calculate_distances.py
+++ day01/test_calculate_distances.py
@@ -4,8 +4,8 @@ import day01.calculate_distances as day1
 class TestCalculateDistances(unittest.TestCase):
 
     def testSamplePart1(self):
-        self.assertEqual(day1.calculate_distances(day1.sample_file_part1), 11)
+        self.assertEqual(day1.calculate_distances(day1.sample_file), 11)
 
     def testInputPart1(self):
-        self.assertEqual(day1.calculate_distances(day1.input_file_part1), 3246517)
+        self.assertEqual(day1.calculate_distances(day1.input_file), 3246517)
         
