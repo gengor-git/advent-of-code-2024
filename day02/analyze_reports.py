1d60bd568252044cbdadb3c5035aad3ade786d5a
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-02 21:36:23 +0100

    Working part 2.

--- day02/analyze_reports.py
+++ day02/analyze_reports.py
@@ -9,22 +9,38 @@ def count_safe_reports(data_file) -> int:
     for line in reports:
         levels = [int(level) for level in line.split()]
         # check if all are increasing
-        if all(levels[i] < levels[i + 1] for i in range(len(levels) - 1)) or all(levels[i] > levels[i + 1] for i in range(len(levels) - 1)):
-            if all(abs(int(levels[i]) - int(levels[i + 1])) < 4 for i in range(len(levels) - 1)):
-                result+=1
+        if is_safe_level(levels):
+            result+=1
     return result
 
-
-def calculate_something2(data_file) -> int:
+def count_safe_reports_with_dampener(data_file) -> int:
     result = 0
-    data = open(data_file).read().strip().splitlines()
-    for entry in data:
-        pass
-
+    reports = open(data_file).read().strip().splitlines()
+    print(len(reports))
+    for line in reports:
+        levels = [int(level) for level in line.split()]
+        # check if all are increasing
+        if is_safe_level(levels):
+            result += 1
+        else:
+            counter = 0
+            for i in range(len(levels)):
+                copy = levels.copy()
+                del copy[i]
+                if is_safe_level(copy):
+                   result += 1
+                   break
     return result
+
+def is_safe_level(levels) -> bool:
+    results = False
+    if all(levels[i] < levels[i + 1] for i in range(len(levels) - 1)) or all(levels[i] > levels[i + 1] for i in range(len(levels) - 1)):
+        if all(abs(int(levels[i]) - int(levels[i + 1])) < 4 for i in range(len(levels) - 1)):
+            results = True
+    return results
 
 if __name__ == "__main__":
-    print(count_safe_reports(sample_file))
-    print(count_safe_reports(input_file))
-    # print(calculate_something2(sample_file))
-    # print(calculate_something2(input_file))
+    # print(count_safe_reports(sample_file))
+    # print(count_safe_reports(input_file))
+    print(count_safe_reports_with_dampener(sample_file))
+    print(count_safe_reports_with_dampener(input_file))
--- day02/test_analyze_reports.py
+++ day02/test_analyze_reports.py
@@ -1,17 +1,17 @@
 import unittest
 import day02.analyze_reports as this_day
 
-class TestCalculateSomething(unittest.TestCase):
+class TestCountSafeReports(unittest.TestCase):
 
     def testSamplePart1(self):
         self.assertEqual(this_day.count_safe_reports(this_day.sample_file), 2)
 
     def testInputPart1(self):
-        self.assertEqual(this_day.count_safe_reports(this_day.input_file), 3246517)
+        self.assertEqual(this_day.count_safe_reports(this_day.input_file), 326)
         
     def testSamplePart2(self):
-        self.assertEqual(this_day.calculate_something2(this_day.sample_file), 11)
+        self.assertEqual(this_day.count_safe_reports_with_dampener(this_day.sample_file), 4)
 
     def testInputPart2(self):
-        self.assertEqual(this_day.calculate_something2(this_day.input_file), 3246517)
+        self.assertEqual(this_day.count_safe_reports_with_dampener(this_day.input_file), 381)
         
