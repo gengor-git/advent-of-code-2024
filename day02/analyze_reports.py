b3cb67ad26a595c5ac785d0f96634f5a7cb2a936
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-02 21:01:13 +0100

    Working part 1.

--- day02/analyze_reports.py
+++ day02/analyze_reports.py
@@ -4,13 +4,13 @@ sample_file = "day02/sample.txt"
 
 def count_safe_reports(data_file) -> int:
     result = 0
-    data = open(data_file).read().strip().splitlines()
-    print(len(data))
-    for entry in data:
-        report = entry.split(" ")
+    reports = open(data_file).read().strip().splitlines()
+    print(len(reports))
+    for line in reports:
+        levels = [int(level) for level in line.split()]
         # check if all are increasing
-        if all(report[i] < report[i + 1] for i in range(len(report) - 1)) or all(report[i] > report[i + 1] for i in range(len(report) - 1)):
-            if all(abs(int(report[i]) - int(report[i + 1])) < 4 for i in range(len(report) - 1)):
+        if all(levels[i] < levels[i + 1] for i in range(len(levels) - 1)) or all(levels[i] > levels[i + 1] for i in range(len(levels) - 1)):
+            if all(abs(int(levels[i]) - int(levels[i + 1])) < 4 for i in range(len(levels) - 1)):
                 result+=1
     return result
 
