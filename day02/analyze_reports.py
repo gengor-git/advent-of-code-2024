0d01095c21fc02d6d041b5ebd84eded81ac58ab6
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-02 23:38:41 +0100

    Streamline if expressions.

--- day02/analyze_reports.py
+++ day02/analyze_reports.py
@@ -26,8 +26,8 @@ def count_safe_reports_with_dampener(data_file) -> int:
 
 def is_safe_level(levels) -> bool:
     results = False
-    if all(levels[i] < levels[i + 1] for i in range(len(levels) - 1)) or all(levels[i] > levels[i + 1] for i in range(len(levels) - 1)):
-        if all(abs(int(levels[i]) - int(levels[i + 1])) < 4 for i in range(len(levels) - 1)):
+    if levels == sorted(levels) or levels == sorted(levels, reverse=True):
+        if all(1<=abs(int(levels[i]) - int(levels[i + 1])) <= 3 for i in range(len(levels) - 1)):
             results = True
     return results
 
