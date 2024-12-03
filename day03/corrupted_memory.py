8a7e7e32722f2348ee50a3f88bf394aa22915b9c
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-03 07:34:12 +0100

    Semi working for sample, but not real data.

--- day03/corrupted_memory.py
+++ day03/corrupted_memory.py
@@ -1,28 +1,34 @@
 import re
 
-input_file = "day03/input.txt"
-sample_file = "day03/sample.txt"
+input_file = 'day03/input.txt'
+sample_file = 'day03/sample.txt'
+sample_file_instr = 'day03/sample2.txt'
 
-reg_pattern = r"mul\(\d{1,3},\d{1,3}\)"
+reg_pattern = r'mul\((\d+),(\d+)\)'
+reg_remove = r'don\'t\(\)[\s\S]+?do\(\)'
 
 def mult_corrputed_memory(data_file) -> int:
     result = 0
-    data = open(data_file).read().strip()
+    data = open(data_file).read()
+    print(data)
+    # remove all from don't() to do()
     matches = re.findall(reg_pattern, data)
-    for values in matches:
-        num = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", values)
-        result+= int(num[0][0]) * int(num[0][1])
+    for x, y in matches:
+        result+= int(x) * int(y)
     return result
 
-def calculate_something2(data_file) -> int:
+def mult_corrputed_instr_memory(data_file) -> int:
     result = 0
-    data = [[int(s) for s in line.split()] for line in open(data_file).read().strip().splitlines()]
-    for entry in data:
-        pass
+    data = open(data_file).read()
+    data = re.sub(reg_remove, '', data)
+    matches = re.findall(reg_pattern, data)
+    for x, y in matches:
+        result+= int(x) * int(y)
     return result
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     print(mult_corrputed_memory(sample_file))
     print(mult_corrputed_memory(input_file))
-    # print(calculate_something2(sample_file))
-    # print(calculate_something2(input_file))
+    print(mult_corrputed_instr_memory(sample_file_instr))
+    print(mult_corrputed_instr_memory(input_file))
