8ae8c830ac3456f08c0f1fd3fc4a0f794d222b6b
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-05 19:33:10 +0100

    Hmmm.

--- day05/print_updates.py
+++ day05/print_updates.py
@@ -47,48 +47,55 @@ def fix_and_sum_mid_page_updates(data_file) -> int:
     data = open(data_file).read().split('\n\n')
     rules = [[int(s) for s in line.split('|')] for line in data[0].splitlines()]
     updates = [[int(s) for s in line.split(',')] for line in data[1].splitlines()]
-    print(len(rules))
-    print(len(updates))
+    print('# update blocks: {}'.format(len(updates)))
+    print(updates)
+    print('# rules: {}'.format(len(rules)))
+    print(rules)
 
-    for updates_block in updates:
-        print('Updates block: {}'.format(updates_block))
-        max_index = len(updates_block)-1
-        page_index = 0
-        rule_broken = False
-        for page in updates_block:
+
+    for update in updates:
+        print('================================================\nUpdates block:   {}'.format(update))
+        max_index = len(update)-1
+        # current_page_index = 0
+        rule_was_broken = False
+        current_page_index = 0
+        while current_page_index < len(update):
+            print('Current     page [ {} ] [ {} ]'.format(current_page_index, update[current_page_index]))
+            # print("Applying rules")
             for rule in rules:
-                if page == rule[0]:
-                    # print("RULEMATCH: This page must be BEFORE {} <- {}".format(page, rule[1]))
+                if update[current_page_index] == rule[0]:
                     # Scan if left of us ISN'T the other page number
-                    for i in range(page_index):
-                        if rule[1] == updates_block[i]:
-                            print("RULEBREAK: page [ {} ] isn't BEFORE <- [ {} ]".format(page, rule[1]))
-                            rule_broken = True
+                    for i in range(current_page_index):
+                        if rule[1] == update[i]:
+                            print("RULEBREAK   page [ {} ] [ {} ] isn't BEFORE <- [ {} ]".format(current_page_index, update[current_page_index], rule[1]))
+                            rule_was_broken = True
                             # Fix the order based on rules
                             # move current page to this index i
-                            updates_block.insert(i, updates_block.pop(page_index))
-                            break
-                elif page == rule[1]:
-                    # print("RULEMATCH: This page must be AFTER  {} -> {}".format(page, rule[0]))
+                            update.insert(i, update.pop(current_page_index))
+                            print('New updates block: {}'.format(update))
+                elif update[current_page_index] == rule[1]:
                     # Scan if right of us ISN'T the other page number
-                    for i in range(page_index, len(updates_block)-1):
-                        if rule[0] == updates_block[i]:
-                            print("RULEBREAK: page [ {} ] isn't AFTER  -> [ {} ]".format(page, rule[0]))
+                    for i in range(current_page_index+1, len(update)):
+                        if rule[0] == update[i]:
+                            print("RULEBREAK   page [ {} ] [ {} ] isn't AFTER  -> [ {} ]".format(current_page_index, update[current_page_index], rule[0]))
                             # Fix the order based on rules
                             # move current page to this index i
-                            updates_block.insert(i, updates_block.pop(page_index))
-                            rule_broken = True
-                            break
-            page_index+=1
-        if rule_broken:
+                            update.insert(i, update.pop(current_page_index))
+                            print('Fixed page order {}'.format(update))
+                            rule_was_broken = True
+            if not rule_was_broken:
+                current_page_index += 1
+            else:
+                current_page_index += 1
+        if rule_was_broken:
             # we assume we fixed it above and only need to grab the mid termin value.
-            result_index = int((len(updates_block)-1)/2)
-            print('Invalid Update mid: [{}]'.format(updates_block[result_index]))
-            result += updates_block[result_index]
+            result_index = int((len(update)-1)/2)
+            print('================================================\nInvalid Update mid: [{}]'.format(update[result_index]))
+            result += update[result_index]
     return result
 
 if __name__ == "__main__":
     # print(sum_mid_page_updates(sample_file))
     # print(sum_mid_page_updates(input_file))
-    print(fix_and_sum_mid_page_updates(sample_file))
-    # print(fix_and_sum_mid_page_updates(input_file))
+    # print(fix_and_sum_mid_page_updates(sample_file))
+    print(fix_and_sum_mid_page_updates(input_file))
