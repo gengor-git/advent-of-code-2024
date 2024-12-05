49cd21ba5ea4ba02ac33d4e54039a1abfd078158
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-05 08:06:41 +0100

    Just some basics.

---
+++ day05/test_printup_dates.py
@@ -0,0 +1,17 @@
+import unittest
+import day05.print_updates as this_day
+
+class TestPrintUpdates(unittest.TestCase):
+
+    def testSamplePart1(self):
+        self.assertEqual(this_day.sum_mid_page_updates(this_day.sample_file), 143)
+
+    def testInputPart1(self):
+        self.assertEqual(this_day.calculate_something(this_day.input_file), 3246517)
+        
+    def testSamplePart2(self):
+        self.assertEqual(this_day.calculate_something2(this_day.sample_file), 11)
+
+    def testInputPart2(self):
+        self.assertEqual(this_day.calculate_something2(this_day.input_file), 3246517)
+        
