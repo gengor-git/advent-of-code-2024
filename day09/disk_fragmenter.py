2c89f7d1c439b467361a16c328d5f457db4f6878
Martin Palmowski <martin.palmowski@gmail.com>
2024-12-09 17:45:11 +0100

    Day 9, part 2 solved.

--- day09/disk_fragmenter.py
+++ day09/disk_fragmenter.py
@@ -1,6 +1,6 @@
+import os
 input_file = "day09/input.txt"
 sample_file = "day09/sample.txt"
-
 
 def defrag_and_checksum_part1(data_file) -> int:
     result = 0
@@ -14,11 +14,54 @@ def defrag_and_checksum_part1(data_file) -> int:
         else:
             for i in range(block):
                 disk.append('.')
-    print('Diskmap:\n{}'.format(diskmap))
-    print('Disk:\n{}'.format(''.join(disk)))
+    # print(disk)
+    defrag(disk)
+    result = get_checksum(disk)
+
 
     return result
 
+def defrag(disk: list) -> list:
+    """
+    Takes the list an defrags it according to the rule. Start at the end and
+    fill those blocks from the front.  
+    Paramters:
+        disk (list): the current disk value
+    Returns:
+        list: defragmented list
+    """
+    complete = False
+    spaces = []
+
+    reversed_index = len(disk)
+    print('Defragging.', end='')
+    for idx, block in enumerate(reversed(disk)):
+        reversed_index -= 1
+        # print('.', end='')
+        if block != '.' and disk.index('.') < reversed_index:
+            # print('{}-{}-{}'.format(idx, block, reversed_index))
+            disk[reversed_index], disk[disk.index('.')] = disk[disk.index('.')], disk[reversed_index]
+
+    print('')
+    # print(disk)
+    return disk
+
+def get_checksum(disk) -> int:
+    checksum = 0
+    for idx, value in enumerate(disk):
+        if value != '.':
+            checksum += idx * int(value)
+
+    return checksum
+
+def cprint(text: str):
+    try:
+        t = os.get_terminal_size()
+    except:
+        t = 45
+    print('─' * t.columns)
+    print(text)
+
 if __name__ == "__main__":
-    print('────────────────────────────────\nPart 1 sample: {}'.format(defrag_and_checksum_part1(sample_file)))
-    # print('────────────────────────────────\nPart 1 input : {}'.format(defrag_and_checksum_part1(input_file)))
+    cprint('Part 1 sample: {}'.format(defrag_and_checksum_part1(sample_file)))
+    cprint('Part 1 input : {}'.format(defrag_and_checksum_part1(input_file)))
--- day09/test_disk_fragmenter.py
+++ day09/test_disk_fragmenter.py
@@ -1,13 +1,13 @@
 import unittest
 import day09.disk_fragmenter as this_day
 
-class TestSomethingSomething(unittest.TestCase):
+class TestDiskFragmenter(unittest.TestCase):
 
     def testSamplePart1(self):
-        self.assertEqual(this_day.defrag_part1(this_day.sample_file), 11)
+        self.assertEqual(this_day.defrag_and_checksum_part1(this_day.sample_file), 1928)
 
     def testInputPart1(self):
-        self.assertEqual(this_day.defrag_part1(this_day.input_file), 3246517)
+        self.assertEqual(this_day.defrag_and_checksum_part1(this_day.input_file), 6519155389266)
         
     def testSamplePart2(self):
         self.assertEqual(this_day.calculate_something2(this_day.sample_file), 11)
